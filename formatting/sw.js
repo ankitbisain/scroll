15f132e713bb4f03d3c9d943f7510e09fdc7a951
kit <kit@dynamic-oit-ip4-wifirestricted04-10-50-10-92.princeton.edu>
2025-11-21 00:56:05 -0500

    reorganize + trim

---
+++ formatting/sw.js
@@ -0,0 +1,10 @@
+const CACHE = "v3";
+const FILES = ["/", "index.html", "style.css"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(FILES)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});
